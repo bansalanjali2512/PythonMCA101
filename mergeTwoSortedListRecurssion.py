ed5de06aabf2e64bb6674a3e506f097d44830f10
bansalanjali2512 <31640108+bansalanjali2512@users.noreply.github.com>
2017-10-27 12:08:58 +0530

    Add files via upload

---
+++ mergeTwoSortedListRecurssion.py
@@ -0,0 +1,21 @@
+mergedList=[]
+def mergeSortedLists(list1,list2,index1=0,index2=0):
+    '''
+    Objective: To merge to sorted lists
+    Input Parameters:
+      list1: First sorted list
+      list2: Second sorted list
+      index1: Index to traverse list1
+      index2: Index to traverse list2
+    Return: None
+    '''
+    #approach: Using recurssion
+
+    if (index1==(len(list1)-1) and index2==(len(list1)-1)):
+        return mergedList      
+    elif ((index1!=len(list1) and index2==(len(list1)-1)) or (list1[index1] <= list2[index2])):
+        mergedList.append(list1[index1])
+        return mergeSortedLists(list1,list2,index1+1,index2)
+    elif ((index1==(len(list1)-1) and index2!=len(list1)) or (list1[index1] > list2[index2])):
+        mergedList.append(list2[index2])
+        return mergeSortedLists(list1,list2,index1,index2+1)
