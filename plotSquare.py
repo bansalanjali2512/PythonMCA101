34d7b99dfa7001339fce151625fe9d7c5f08c512
Anjali Bansal <anjali.mca17.du@gmail.com>
2017-11-08 23:59:31 +0530

    Update plotSquare.py
    Update

--- plotSquare.py
+++ plotSquare.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-def recSquare(x,y,size=0):
+def recursivePlotSquare(x,y,size=0):
   '''
   Objective: To plot multiple squares
   Input Parameters: x, y - lists of x coordinates and y
@@ -11,14 +11,14 @@ def recSquare(x,y,size=0):
 
   x = [0, size, size, 0, 0]
   y = [0, 0, size, size, 0]
-  square(x,y)
 
   if (size == 1):
     return
   else:
-    return recSquare(x,y,size-1)
+    plotSquare(x,y)
+    return recursivePlotSquare(x,y,size-1)
 
-def square(x, y):
+def plotSquare(x, y):
   '''
   Objective: To plot a square
   Input Parameters: x, y - lists of x coordinates and y
@@ -40,7 +40,7 @@ def main():
 
   x = [0, size, size, 0, 0]
   y = [0, 0, size, size, 0]
-  recSquare(x, y, size)
+  recursivePlotSquare(x, y, size)
   plt.title('Square')
   plt.axis([min(x)-1, max(x)+1, min(y)-1, max(y)+1])
   plt.grid()
