2ee10fd119a577715a737a5d467dd42075960847
Unknown <anjali.mca17.du@gmail.com>
2017-11-27 23:00:06 +0530

    Python built in functions.

---
+++ builtinFunctions.py
@@ -0,0 +1,36 @@
+#Using Built-In Functions
+
+#input function
+print()
+print('** Input Function **')
+name = input('Enter your name: ')
+print('Your name is: ', name)
+
+#eval function
+print()
+print('** Eval Function **')
+print("eval('15+10'): ", eval('15+10'))
+
+#print function
+print()
+print('** Print Function **')
+print('2 + 5 = ', 2 + 5)
+print('Hello', name, ' !')
+
+#type function
+print()
+print('** Type Function **')
+print('type(15): ', type(15))
+print('type(15.5): ', type(15.5))
+print('type("hello"): ', type("hello"))
+print('type(int): ', type(int))
+print('type([]): ', type([]))
+
+#round function
+print()
+print('** Round Function **')
+print('round(89.625,2): ', round(89.625,2))
+print('round(89.625): ', round(89.625))
+print('round(89.625,0): ', round(89.625,0))
+print('round(-89.625): ', round(-89.625))
+
