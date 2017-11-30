70beea021359f4bfdbe35c106b4b9bcfce16ef84
Unknown <anjali.mca17.du@gmail.com>
2017-11-30 23:50:32 +0530

    StartPython.py

---
+++ StartPython.py
@@ -0,0 +1,19 @@
+#StartPython.py
+#AboutMe
+
+def myName():
+    print("My Name is Anjali")
+
+def myCourse():
+	print("I'm pursuing MCA")
+
+def mySchool():
+    print("My school is Dept. of Comp. Sc., University of Delhi")
+
+def aboutMe():
+	myName()
+	myCourse()
+	mySchool()
+
+#Calling aboutMe()
+aboutMe()
