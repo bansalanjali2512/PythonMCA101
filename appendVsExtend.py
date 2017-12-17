287b8fc4053804d31f9bae8ff15b1ac630159987
Anjali Bansal <anjali.mca17.du@gmail.com>
2017-12-17 23:46:37 +0530

    Update appendVsExtend.py

--- appendVsExtend.py
+++ appendVsExtend.py
@@ -6,8 +6,16 @@ def func1(bar = []):
 	 bar.append('xyz') 
 	 return bar 
 print(func1())
+'''
+Output:
+['xyz']
+'''
 
 def func2(bar = []):
 	 bar.extend('xyz') 
 	 return bar
 print(func2())
+'''
+Output:
+['x','y','z']
+'''
